98a60047ab942c1c71a3f62f0fae711e0b9845e9
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-12-08 16:38:27 +0530

    .

---
+++ Redux_practice/practice_task/q3/question.js
@@ -0,0 +1,2 @@
+// Basic Form Handling
+// Create a form with Redux for handling input fields such as name and email. Implement actions to update the state with form values and display the submitted data.
