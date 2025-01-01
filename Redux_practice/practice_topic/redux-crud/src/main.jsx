38a90ca104026fe342b4b134ca4fb5b4a2479287
Monish Khambalkar <monishkhambalkar@gmail.com>
2025-01-01 20:00:27 +0530

    .

---
+++ Redux_practice/practice_topic/redux-crud/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+// https://chatgpt.com/share/676cc331-b3f8-8003-a17d-59375918bd10
