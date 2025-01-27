8ce3784d264933e8ef04d93d2e836ba6cadc7355
Monish Khambalkar <monishkhambalkar@gmail.com>
2025-01-27 08:44:29 +0530

    add q5

---
+++ hooks/app-useContext/q5/question.js
@@ -0,0 +1,3 @@
+// Advanced: Implement a permissions system in a role-based app.
+// Use PermissionsContext to define access permissions based on user roles.
+// Components use useContext to access permission data and conditionally render based on user roles
